5cf548c421f9f5871058a73aaa94986f86244b93
Jonas Tolentino <jonas.tolentino@objective.com.br>
2022-09-04 22:40:03 -0300

    feat: add theme into storage

--- App.tsx
+++ App.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ThemeProvider } from 'styled-components/native'
 import { darkTheme, lightTheme } from '@assets/theme/theme'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import Button from '@components/atoms/Button'
 import PageHome from '@components/pages/Home'
 import SafeAreaView from '@components/atoms/SafeAreaView'
 
 export default function App() {
-	const [theme, setTheme] = React.useState<string>('light')
+	const [theme, setTheme] = useState<string>('light')
+  const getTheme = async () => {
+    try {
+      const themeValue = await AsyncStorage.getItem('@theme')
+      if (themeValue) setTheme(themeValue)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  useEffect(() => {
+    getTheme()
+  }, [])
   const toggleTheme = async () => {
     const themeValue = theme === 'dark' ? 'light' : 'dark'
-    setTheme(themeValue)
+    try {
+      await AsyncStorage.setItem('@theme', themeValue)
+      setTheme(themeValue)
+    } catch (err) {
+      console.error(err)
+    }
   }
 
 	return (
